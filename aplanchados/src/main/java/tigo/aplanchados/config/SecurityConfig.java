cbb8b59ca7155da7fcd94b71e932aa218e61065f
Andrés Pino <andres@Andress-MacBook-Pro.local>
2024-09-20 11:45:34 -0500

    feat: integrate Spring Security for user authentication

--- aplanchados/src/main/java/tigo/aplanchados/config/SecurityConfig.java
+++ aplanchados/src/main/java/tigo/aplanchados/config/SecurityConfig.java
@@ -1,31 +1,45 @@
 package tigo.aplanchados.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorizeRequests -> 
-                authorizeRequests
-                    .requestMatchers("**").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .loginPage("/login")
-                .permitAll()
-            )
-            .logout(logout -> logout
-                .permitAll()
-            );
 
-        return http.build();
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/login","/user/create", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "/home", "/user/create").permitAll()
+                        .requestMatchers("/dashboard", "/manager", "/roles").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                );
+
+    return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
