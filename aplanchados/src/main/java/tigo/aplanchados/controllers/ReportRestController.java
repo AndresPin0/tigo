d2a344c326918da385146abaf09f9e311de073f6
Electromayonaise <agazazaca@gmail.com>
2024-09-22 11:36:18 -0500

    Upload view to upload file, placeholder method on controller

--- aplanchados/src/main/java/tigo/aplanchados/controllers/ReportRestController.java
+++ aplanchados/src/main/java/tigo/aplanchados/controllers/ReportRestController.java
@@ -1,8 +1,12 @@
 package tigo.aplanchados.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpServletResponse;
 import tigo.aplanchados.services.interfaces.ReportService;
@@ -25,5 +29,17 @@ public class ReportRestController {
         response.setHeader(headerKey, headerValue);
 
         reportService.generateDailyExcel(response);
+    }
+
+     @PostMapping("/upload-excel")
+    public ResponseEntity<String> uploadExcelFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // Call the ReplaceForName method to handle the Excel reading logic
+            // reportService.ReplaceForMethodName(file);
+            return ResponseEntity.ok("File uploaded and processed successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("File upload failed");
+        }
     }
 }
