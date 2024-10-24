9d3847d8644d8c276d33a46dd8a5ad5bd477edfe
jul109 <pipechiquito@protonmail.com>
2024-10-24 15:28:15 -0500

    user implements user details

--- aplanchados/src/main/java/tigo/aplanchados/model/User.java
+++ aplanchados/src/main/java/tigo/aplanchados/model/User.java
@@ -1,19 +1,32 @@
 package tigo.aplanchados.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.OverridesAttribute;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.io.Serializable;
 import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
 @Table(name="app_user")
 @Data
-public class User implements Serializable {
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable, UserDetails {
     @Id
     private Long id;
     private String name;
@@ -29,4 +42,34 @@ public class User implements Serializable {
 
     @OneToMany(mappedBy = "user")
     private List<Expense> expenses;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        
+        return List.of(new SimpleGrantedAuthority("AUTORIDAD"));
+    }
+
+
+      @Override
+    public boolean isAccountNonExpired() {
+		return true;
+	}
+
+    @Override
+    public String getUsername() {
+        return "Hola";
+    }
+    
+    public boolean isAccountNonLocked() {
+		return true;
+	}
+
+    public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	public boolean isEnabled() {
+		return true;
+	}
+    
 }
