27dda14ff650779c8d7720e85a2ce73ed46f5da7
jul109 <115905294+jul109@users.noreply.github.com>
2024-11-07 15:03:26 -0500

    router

--- aplanchados-front/src/routes/Router.jsx
+++ aplanchados-front/src/routes/Router.jsx
@@ -1,9 +1,11 @@
 import Home from "../pages/Home";
+import Register from "../pages/Register";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 const routes = createRoutesFromElements(
-    <Route path="/" element={<Home />}>
-        
-    </Route>
-)
+    <>
+    <Route path="/" element={<Home />} /> 
+    <Route path="/register" element={<Register />} />
+    </>
+);
 
 export const router = createBrowserRouter(routes, { basename: '/'});
