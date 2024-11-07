ff1dddece016c622c731f2c2944089b047bb96c8
jul109 <115905294+jul109@users.noreply.github.com>
2024-11-06 19:59:57 -0500

    cors

--- aplanchados-front/src/App.js
+++ aplanchados-front/src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { TEST_URL} from './config/config';
 
 function App() {
-  
+  console.log(TEST_URL);
+  console.log("Alo");
+  fetch(TEST_URL).then((response)=>{
+    console.log("HICE FETCH prueba");
+    console.log(response.json);
+  });
+
+  axios.get(TEST_URL).then(()=>{
+    console.log("Hola");
+  }).catch((e)=>{
+    console.log(e);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
