746599c50dd979d108ba5fbd177427d74071a5fd
jul109 <115905294+jul109@users.noreply.github.com>
2024-11-07 12:39:45 -0500

    axios

--- aplanchados-front/src/App.js
+++ aplanchados-front/src/App.js
@@ -1,40 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import { TEST_URL} from './config/config';
+import { axiosInstance } from './services/axios';
 
 function App() {
-  console.log(TEST_URL);
-  console.log("Alo");
-  fetch(TEST_URL).then((response)=>{
-    console.log("HICE FETCH prueba");
-    console.log(response.json);
-  });
 
-  axios.get(TEST_URL).then(()=>{
-    console.log("Hola");
-  }).catch((e)=>{
-    console.log(e);
-  });
+  /*
+    fetch(TEST_URL).then((response)=>{
+      console.log("HICE FETCH prueba");
+      console.log(response.json);
+    });
+  
+  */
+ axiosInstance.get("/auth/test").then((response)=>{
+  console.log(response);
+ });
+  
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
 }
 
 export default App;
