c09ea388e72f49d573cbf8905d03c8543aed2f96
jul109 <115905294+jul109@users.noreply.github.com>
2024-11-07 20:13:19 -0500

    material ui

--- aplanchados-front/src/pages/Home.jsx
+++ aplanchados-front/src/pages/Home.jsx
@@ -1,5 +1,20 @@
-export default function  Home (){
-    return(
-        <p>Este es el home</p>
+import TextField from '@mui/material/TextField';
+
+
+export default function Home() {
+    return (
+        <div>
+
+            <p>Username</p>
+            <p>Password</p>
+            <TextField
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+        />
+        </div>
+
     );
 }
