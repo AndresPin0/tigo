25f2c142d004f96f39a3f4b31255ad5461cdcc13
jul109 <115905294+jul109@users.noreply.github.com>
2024-11-08 13:47:23 -0500

    register

--- aplanchados-front/src/pages/Register.jsx
+++ aplanchados-front/src/pages/Register.jsx
@@ -1,6 +1,52 @@
-function Register (){
+import { TextField } from "@mui/material";
+
+import Stack from '@mui/material/Stack';
+
+import Container from '@mui/material/Container';
+
+import Button from '@mui/material/Button';
+function Register() {
     return (
-        <p>Register</p>
+
+        <div>
+            <Container maxWidth="sm">
+                <Stack spacing={2}>
+
+
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Nombre"
+                    />
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Apellido"
+                    />
+
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="ID"
+                    />
+
+                    <TextField
+                        id="filled-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        variant="filled"
+                        required
+                    />
+
+
+
+                    <Button variant="contained"> Registrarse</Button>
+                </Stack>
+
+            </Container>
+        </div>
+
     );
 }
 export default Register;
