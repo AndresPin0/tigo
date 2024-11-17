bd95d558ad78731e92f37f3ce7f98829312f3bd0
tyronemab <jhonatancastanoibarra@gmail.com>
2024-11-17 07:54:50 -0500

    Merge fix

--- aplanchados-front/src/routes/Router.jsx
+++ aplanchados-front/src/routes/Router.jsx
@@ -13,55 +13,6 @@ import NavBar from '../components/NavBar';
 import BackArrow from '../components/BackArrow';
 
 const routes = createRoutesFromElements(
-<<<<<<< HEAD
-    <>
-        <Route path="/" element={<Landing />} >
-        <Route index element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} /> 
-        </Route>
-     <Route 
-     path="manager" 
-     element={
-       <ProtectedRoute requiredPermission="MANAGE SYSTEM">
-         <ManagerPage />
-       </ProtectedRoute>
-     } 
-   />
-   <Route 
-     path="expense" 
-     element={
-       <ProtectedRoute requiredPermission="ADD EXPENSE">
-         <ExpensePage />
-       </ProtectedRoute>
-     } 
-   />
-   <Route 
-     path="income/create" 
-     element={
-       <ProtectedRoute requiredPermission="ADD INCOME">
-         <IncomePage />
-       </ProtectedRoute>
-     } 
-   />
-   <Route 
-     path="report/upload-excel" 
-     element={
-       <ProtectedRoute requiredPermission="GENERATE REPORT">
-         <ReportPage />
-       </ProtectedRoute>
-     } 
-   />
-   <Route 
-     path="reports/monthly" 
-     element={
-       <ProtectedRoute requiredPermission="GENERATE REPORT">
-         <MonthlyReportPage />
-       </ProtectedRoute>
-     } 
-   />
- </>
-=======
   <>
     <Route path="/" element={<Landing />} >
       <Route index element={<Login />} />
@@ -108,8 +59,15 @@ const routes = createRoutesFromElements(
         </ProtectedRoute>
       } 
     />
+    <Route 
+     path="reports/monthly" 
+     element={
+       <ProtectedRoute requiredPermission="GENERATE REPORT">
+         <MonthlyReportPage />
+       </ProtectedRoute>
+     } 
+   />
   </>
->>>>>>> 0a47ddb513176aad256d0b6fceead9f223f6731f
 );
 
 export const router = createBrowserRouter(routes, { basename: '/' });
